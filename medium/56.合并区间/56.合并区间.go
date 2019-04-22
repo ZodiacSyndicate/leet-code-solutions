1b3ee77023334418823ad23aac7542de0cc8d8b7
zodiacsyndicate <zodiacsyndicate@163.com>
2019-04-22 15:35:12 +0800

    medium #59

--- medium/56.合并区间/56.合并区间.go
+++ medium/56.合并区间/56.合并区间.go
@@ -12,20 +12,20 @@
  * Testcase Example:  '[[1,3],[2,6],[8,10],[15,18]]'
  *
  * 给出一个区间的集合，请合并所有重叠的区间。
- * 
+ *
  * 示例 1:
- * 
+ *
  * 输入: [[1,3],[2,6],[8,10],[15,18]]
  * 输出: [[1,6],[8,10],[15,18]]
  * 解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
- * 
- * 
+ *
+ *
  * 示例 2:
- * 
+ *
  * 输入: [[1,4],[4,5]]
  * 输出: [[1,5]]
  * 解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。
- * 
+ *
  */
 /**
  * Definition for an interval.
@@ -34,7 +34,44 @@
  *	   End   int
  * }
  */
-func merge(intervals []Interval) []Interval {
-    
+import "sort"
+
+type Intervals []Interval
+
+func (this Intervals) Len() int {
+	return len(this)
 }
 
+func (this Intervals) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+func (this Intervals) Less(i, j int) bool {
+	return this[i].Start < this[j].Start
+}
+
+func merge(intervals []Interval) []Interval {
+	res := []Interval{}
+	if len(intervals) == 0 {
+		return res
+	}
+	sort.Sort(Intervals(intervals))
+	cur := intervals[0]
+	for i := 1; i < len(intervals); i++ {
+		if cur.End >= intervals[i].Start {
+			cur.End = max(intervals[i].End, cur.End)
+		} else {
+			res = append(res, cur)
+			cur = intervals[i]
+		}
+	}
+	res = append(res, cur)
+	return res
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
+	}
+	return a
+}
--- medium/59.螺旋矩阵-ii/59.螺旋矩阵-ii.go
+++ medium/59.螺旋矩阵-ii/59.螺旋矩阵-ii.go
@@ -12,9 +12,9 @@
  * Testcase Example:  '3'
  *
  * 给定一个正整数 n，生成一个包含 1 到 n^2 所有元素，且元素按顺时针顺序螺旋排列的正方形矩阵。
- * 
+ *
  * 示例:
- * 
+ *
  * 输入: 3
  * 输出:
  * [
@@ -22,9 +22,36 @@
  * ⁠[ 8, 9, 4 ],
  * ⁠[ 7, 6, 5 ]
  * ]
- * 
+ *
  */
 func generateMatrix(n int) [][]int {
-    
+	matrix := [][]int{}
+	for i := 0; i < n; i++ {
+		arr := []int{}
+		for j := 0; j < n; j++ {
+			arr = append(arr, -1002345)
+		}
+		matrix = append(matrix, arr)
+	}
+	directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	x, y, i, k, flag := 0, 0, 0, 1, false
+	for {
+		if x < n && y < n && x >= 0 && y >= 0 && matrix[x][y] == -1002345 {
+			flag = false
+			matrix[x][y] = k
+			k++
+		} else {
+			if flag {
+				break
+			}
+			flag = true
+			x -= directions[i][0]
+			y -= directions[i][1]
+			i = (i + 1) % 4
+		}
+		x += directions[i][0]
+		y += directions[i][1]
+	}
+	return matrix
 }
 
